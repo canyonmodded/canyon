68e3eab9e04a0a840d59f4eb79491181b6a9fe5e
Andrew Steinborn <git@steinborn.me>
2020-05-21 00:40:12 -0400

    Include minimal fastutil in a platform-independent way

---
+++ Canyon-MinFastUtil/src/main/java/com/canyonmodded/FastutilUsages.java
@@ -0,0 +1,9 @@
+package com.canyonmodded;
+
+import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap;
+
+public class FastutilUsages {
+    public static void dummy() {
+        new Long2LongOpenHashMap();
+    }
+}
