355157e5d7c25254a9f590dc2cccde4f2815e597
Andrew Steinborn <git@steinborn.me>
2020-05-21 00:47:15 -0400

    Giving up, use full-fat fastutil.

--- Canyon-MinFastUtil/src/main/java/com/canyonmodded/FastutilUsages.java
+++
@@ -1,9 +0,0 @@
-package com.canyonmodded;
-
-import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap;
-
-public class FastutilUsages {
-    public static void dummy() {
-        new Long2LongOpenHashMap();
-    }
-}
